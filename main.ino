537d1a82748d1e82cf8983bb05ae66e916774ded
zellio22 <49569121+zellio22@users.noreply.github.com>
2022-07-21 16:10:57 +0200

    Add files via upload

--- main.ino
+++ main.ino
@@ -1,11 +1,22 @@
 
-#include <LiquidCrystal_I2C.h>
+
+
+#include <LiquidCrystal_I2C.h> 	//Lib I2C en focntion de l'ecrant
+#include <ParallaxLCD.h>		//Lib ParallaxLCD pour LCD serial 
 #include <arduino.h>
+#include <SoftwareSerial.h>		//Voir la necessitée d'include cette LIB normalement non 
 
 //reste a faire 
 //une gestion du relai d'exitation tempo 
 //une raz de toutes les variables global car je code comme un porc 
 //la mesure des tempos a la d'esexitation
+//lib paralax https://github.com/iamthechad/parallax_lcd/blob/master/examples/LCD_2x16/LCD_2x16.ino
+
+
+#define ROWS 4
+#define COLS 10
+//ParallaxLCD lcd(2,ROWS,COLS); // choix pin TX, Lignes, colone
+
 
 LiquidCrystal_I2C lcd(0x27,20,4);	// adresse du LCD I2C
 
@@ -19,28 +30,34 @@ int tempo = 0;						//Variable a 1 pour tenmpo en mesure
 
 //def des pin Entrée /sortie
 
-int EX = 31; 					//pin comutateur choix Exit d'ezexit
-int D_EX = 33;					//pin comutateur choix Exit d'ezexit
-int in_tempo_409 = 35;			//defini la pin utiliser pour le 409
-int in_tempo_401 = 37;			//defini la pin utiliser pour le 401
-int out_relay = 39;
+int EX = 0; 					//pin comutateur choix Exit d'ezexit
+int D_EX = 1;					//pin comutateur choix Exit d'ezexit
+int in_tempo_409 = 2;			//defini la pin utiliser pour le 409
+int in_tempo_401 = 3;			//defini la pin utiliser pour le 401
+int out_relay = 4;
 
 
 
 int choix()
 {
-	if (digitalRead(EX)==0 && digitalRead(D_EX)==1){ return 1;} // CHOIX TEMPO exitation 
-	if (digitalRead(EX)==1 && digitalRead(D_EX)==0){ return 2;} // CHOIX TEMPO dezesxit
+	if (digitalRead(EX)==1 && digitalRead(D_EX)==0){ return 1;} // CHOIX TEMPO exitation 
+	if (digitalRead(EX)==0 && digitalRead(D_EX)==1){ return 2;} // CHOIX TEMPO dezesxit
 	else return 0;
 }
 
 void setup() // la boucle setup  est executer unique a la mise en service de l'arduino
 {
-
+	//Set du LCD i2c
 	lcd.init();			// initialisation de l'ecrant LCD 
 	lcd.backlight();	// allumage du retro-eclairage 
 	lcd.setCursor(0,0); // positionement du curseur 
 	Serial.begin(9600); // initialise la connexion série à 9600 bauds
+	
+	//Set du LCD serial 
+	//lcd.setup();
+	//lcd.backLightOn();//retro-eclairage
+	//delay(1000);//Wait du LCD 
+	//lcd.empty();//Clear de l'ecran
 	
 	//definition du roles des E/S 
 	pinMode(in_tempo_409, INPUT); // 409 
@@ -64,14 +81,12 @@ void loop()
 
 	{
 		case 0: // corespond a un " if choix ==0 "et nexecute pas la suite du switch
-			lcd.clear();
-			lcd.setCursor(0,0);
-			lcd.print("Choix Incorect");
-			break;
+
 		{
 			
 			lcd.setCursor(0,0);
 			lcd.print("Un seul choix   ");//quand tu vien ici c'est que tu a choisi un choix qui n'existe pas ... merci l'ia
+			//lcd.at(0,0,"Choix Incorect");//Pour LCD Serial
 			break;
 		}
 		
@@ -79,6 +94,7 @@ void loop()
 		{
 			lcd.setCursor(0,0);// on positionement du curseur a la ligne 0 colonne 0
 			lcd.print("TEMPO exitation ");// on affiche le message
+			//lcd.at(0,0,"TEMPO exitation ");
 			
 			if(digitalRead(in_tempo_409)==1 && tempo==0 ) // si l'entrée est en HIGH et que la tempo est off
 			{
